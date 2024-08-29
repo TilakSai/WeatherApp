20c8e410e7753d10db3454dfd6d1f81ae5722b2b
Nikhil D <dnkhilc@gmail.com>
2024-08-29 13:23:26 -0500

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Nikhil Doppalapudi on 8/29/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
